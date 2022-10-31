1575c14b6a98040a0cd51208719e3ec257f351e9
Pas <askpeevee@gmail.com>
2022-10-31 23:07:25 +0000

    Js code

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,15 +1,59 @@
+// All JavaScript need to linked to HTML
+
+/*
 let js = "amazing";
-
-if(js === "amazing") alert("JavaScript is Fun!");
-
-40 + 8 + 23 -10;
-
 console.log(40 + 8 + 23 - 10);
 
+console.log("Jonas");
+console.log(23);
+
+let firstName = "Matilda";
+
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+let country = "Indonesia";
+let continent = "Asia";
+let population = "500000"
+
+console.log(country);
+console.log(continent);
+console.log(population);
+
+ In line script means you dont need to create a separate file
+
+Carmel Case is the used for variable in JS naming convention
+
+How to see results for code in the Browser by using  called console.log 
+*/
 
 
-// All JavaScript need to linked to HTML 
 
-// In line script means you dont need to create a separate file 
 
-// How to see results for code in the Browser by using function called console.log 
+/*There are eight basic data types in JavaScript. They are:
+
+Data Types	Description	Example
+
+String	represents textual data	'hello', "hello world!" etc
+
+Number	an integer or a floating-point number	3, 3.234, 3e-2 etc.
+
+BigInt	an integer with arbitrary precision	900719925124740999n , 1n etc.
+
+Boolean	Any of two values: true or false	true and false
+
+undefined	a data type whose variable is not initialized	let a;
+
+null	denotes a null value	let a = null;
+
+Symbol	data type whose instances are unique and immutable	let value = Symbol('hello');
+
+Object	key-value pairs of collection of data	let student = { };
+
+Here, all data types except Object are primitive data types, whereas Object is non-primitive.
+*/
+
+true;
+console.log("javascriptIsFun");
+let javascriptIsFun = true;

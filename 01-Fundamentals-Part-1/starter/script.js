065846ed1de46b9acb559ddaf65866e5719313f4
Pas <askpeevee@gmail.com>
2022-10-31 17:30:32 +0000

    Js code

---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,15 @@
+let js = "amazing";
+
+if(js === "amazing") alert("JavaScript is Fun!");
+
+40 + 8 + 23 -10;
+
+console.log(40 + 8 + 23 - 10);
+
+
+
+// All JavaScript need to linked to HTML 
+
+// In line script means you dont need to create a separate file 
+
+// How to see results for code in the Browser by using function called console.log 

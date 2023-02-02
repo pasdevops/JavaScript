d568f62cbe9f52f4286d5db5c8796a40df07d981
Pas <askpeevee@gmail.com>
2023-02-02 18:25:57 +0000

    js  updated  commit

--- my_note/starter/script.js
+++ my_note/starter/script.js
@@ -1043,3 +1043,54 @@ hello = () => {
 //   calcAge(years[years.length - 1]),
 // ];
 // console.log(ages);
+
+// JavaScript Array Methods and Properties
+
+// const friends = ["Micheal", "John", "Peter"];
+// const newLength = friends.push("Jay"); // This is a method used to add ELEMENT to an ARRAY.
+// Push is a function and it can return something
+// You can pass arguments into functions
+// Push function is used to retune a value
+// To capture the new value, you can create a new VARIABLE for it
+// console.log(friends);
+// console.log(newLength);
+
+// friends.unshift("John");
+// console.log(friends);
+
+// Remove Element
+// friends.pop("Jay");
+// console.log(friends);
+
+// console.log(friends.indexOf("Stephen")); // This is not on the Array list -1
+// console.log(friends.indexOf("John"));
+
+// friends.push(23);
+// console.log(friends.includes("Stephen")); // This method uses equality
+// console.log(friends.includes("Peter")); // This method uses equality
+// console.log(friends.includes("23")); // This method uses equality
+
+// ARRAY Coding Challenge #2
+
+// Steven is still building his tip calculator, using the same rules as before: Tip 15% of
+// the bill if the bill value is between 50 and 300, and if the value is different, the tip is
+// 20%.
+
+// Your tasks:
+
+// 1. Write a function 'calcTip' that takes any bill value as an input and returns
+// the corresponding tip, calculated based on the rules above (you can check out
+// the code from first tip calculator challenge if you need to). Use the function
+// type you like the most. Test the function using a bill value of 100
+
+// 2. And now let's use arrays! So create an array 'bills' containing the test data
+// below
+
+// 3. Create an array 'tips' containing the tip value for each bill, calculated from
+// the function you created before
+
+// 4. Bonus: Create an array 'total' containing the total values, so the bill + tip
+
+// Test data: 125, 555 and 44
+
+const calcTip = [125, 555, 44];
